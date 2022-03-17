4a20cd2f7fdb1cbfcda9a40dc938598d24cf79bf
Denzel <dbartolaba123@gmail.com>
2022-03-17 15:49:49 -0700

    created controller and test for insert

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,14 @@ describe('handofRes routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should create cars', async () => {
+    const cars = { model: 'tacoma', make: 'toyota', year: 2016};
+    const res = await request(app).post(`/api/v1/cars`).send(cars);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...cars,
+    });
+  });
 });
