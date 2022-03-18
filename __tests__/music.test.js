8730febfd3cd47234d7ab956607e68c68f93b5cd
Denzel <dbartolaba123@gmail.com>
2022-03-18 16:10:59 -0700

    got all of my tests passing!!!!!!

--- __tests__/music.test.js
+++ __tests__/music.test.js
@@ -24,42 +24,42 @@ describe('handofRes routes', () => {
   });
 
 
-//   it('should be able to list music by id', async () => {
-//     const music = await Music.insert({ song: 'La Romana', artist: 'Bad Bunny', genre: 'reggaeton', year: 2019 });
-//     const res = await request(app).get(`/api/v1/music/${music.id}`);
+  it('should be able to list music by id', async () => {
+    const music = await Music.insert({ song: 'La Romana', artist: 'Bad Bunny', genre: 'reggaeton', year: 2019 });
+    const res = await request(app).get(`/api/v1/music/${music.id}`);
 
-//     expect(res.body).toEqual(music);
-//   });
+    expect(res.body).toEqual(music);
+  });
 
-//   it('should get a list of all music', async () => {
-//     const music = await Music.getAll();
-//     const res = await request(app).get(`/api/v1/music`);
+  it('should get a list of all music', async () => {
+    const music = await Music.getAll();
+    const res = await request(app).get(`/api/v1/music`);
 
-//     expect(res.body).toEqual(music);
-//   });
+    expect(res.body).toEqual(music);
+  });
 
-// it('should update music by id', async () => {
-//   const music = await Music.insert({ song: 'Chosen', artist: 'Blxst', genre: 'hip/hop', year: 2020 });
-//   const res = await request(app)
-//   .patch(`/api/v1/music/${music.id}`)
-//   .send({ song: 'Got it All', artist: 'Blxst', genre: 'hip/hop', year: 2019});
+it('should update music by id', async () => {
+  const music = await Music.insert({ song: 'Chosen', artist: 'Blxst', genre: 'hip/hop', year: 2020 });
+  const res = await request(app)
+  .patch(`/api/v1/music/${music.id}`)
+  .send({ song: 'Got it All', artist: 'Blxst', genre: 'hip/hop', year: 2019});
 
-//   const expected = {
-//     id: expect.any(String),
-//     song: 'Got it All',
-//     artist: 'Blxst',
-//     genre: 'hip/hop',
-//     year: 2019,
-//   };
-//   expect(res.body).toEqual(expected);
-//   expect(await Music.getById(music.id)).toEqual(expected);
-// });
+  const expected = {
+    id: expect.any(String),
+    song: 'Got it All',
+    artist: 'Blxst',
+    genre: 'hip/hop',
+    year: 2019,
+  };
+  expect(res.body).toEqual(expected);
+  expect(await Music.getById(music.id)).toEqual(expected);
+});
 
-// it('should be able to delete music by id', async () => {
-//   const music = await Music.insert({ song: 'Low', artist: 'FloRida', genre: 'hip/hop', year: 2008});
-//   const res = await request(app).delete(`/api/v1/music/${music.id}`);
+it('should be able to delete music by id', async () => {
+  const music = await Music.insert({ song: 'Low', artist: 'FloRida', genre: 'hip/hop', year: 2008});
+  const res = await request(app).delete(`/api/v1/music/${music.id}`);
 
-//   expect(res.body).toEqual(music);
-//   expect(await Music.getById(music.id)).toBeNull();
-// });
+  expect(res.body).toEqual(music);
+  expect(await Music.getById(music.id)).toBeNull();
+});
 });
